80806d66ad326f8cf6b52492fd4d73d581f98f78
Javier <javier@javier-Lenovo-G480.(none)>
2015-09-14 16:02:37 -0400

    imagenes en pagina productos

--- django2/urls.py
+++ django2/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^productos/', include('productos.urls')),
+    url(r'^$', include('productos.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
 ]
--- productos/models.py
+++ productos/models.py
@@ -4,6 +4,8 @@ class producto(models.Model):
 	nombre = models.CharField(max_length=50)
 	descripcion = models.CharField(max_length=150)
 	p_neto = models.IntegerField()
+	p_iva = models.IntegerField()
+	p_venta = models.IntegerField()
 	imagen = models.ImageField(upload_to='imagenes')
 	p_web = models.URLField()
 
