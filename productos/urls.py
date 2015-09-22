cc4aa329213b1125134bdd2c95e0596bdb86ad10
Javier <javier@javier-Lenovo-G480.(none)>
2015-09-22 17:14:21 -0400

    agregada funcion convertir pdf a productos

--- productos/urls.py
+++ productos/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     url(r'^(?P<producto_id>[0-9]+)/$', views.detalle, name='detalle'),
+    url(r'^to-pdf/(?P<pk>[0-9]+)/$', views.convertir_pdf, name='to-pdf'),
 ]
 """ # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
